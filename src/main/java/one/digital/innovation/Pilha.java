89d22b59d8d6d232acc41f85bcd13e5a77ba375d
eduhenripontes <edu.henri@gmail.com>
2022-01-03 17:18:56 -0300

    Utilizacao da classe Pilha

--- src/main/java/one/digital/innovation/Pilha.java
+++ src/main/java/one/digital/innovation/Pilha.java
@@ -8,20 +8,19 @@ public class Pilha {
         this.refNoEntradaPilha = null;
     }
 
-    public No Push(No novoNo){
+    public void push(No novoNo) {
         No refAuxiliar = refNoEntradaPilha;
         refNoEntradaPilha = novoNo;
         refNoEntradaPilha.setRefNo(refAuxiliar);
+}
 
-    }
-
-    public No Pop(){
-        if(this.isEmpty()){
+    public No pop(){
+        if(!this.isEmpty()){
             No noPoped = refNoEntradaPilha;
             refNoEntradaPilha = refNoEntradaPilha.getRefNo();
             return noPoped;
         }
-        return null
+        return null;
 
     }
     public No top(){
@@ -29,9 +28,31 @@ public class Pilha {
     }
 
     public boolean isEmpty() {
-        if (refNo == null) {
+       /* if (refNo == null) {
             return true;
-        } else return false;
-        //return refNo == null ? true: false;
+        } else return false;*/
+
+        return refNoEntradaPilha == null ? true : false;
+    }
+
+    @Override
+    public String toString() {
+
+        String stringRetorno = "--------------\n";
+        stringRetorno += "   Pilha\n";
+        stringRetorno += "--------------\n";
+
+        No noAuxiliar = refNoEntradaPilha;
+
+        while(true){
+            if(noAuxiliar != null){
+                stringRetorno += "[No{dado= " + noAuxiliar.getDado() + "}]\n";
+                noAuxiliar = noAuxiliar.getRefNo();
+            }else{
+                break;
+            }
+        }
+        stringRetorno += "==============\n";
+        return stringRetorno;
     }
 }
